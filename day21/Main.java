906fdb6b5cb36583fbf94cce56190c0db1fd7a22
Cayley <cayley@dr.com>
2023-02-13 18:04:49 +0900

    HW 1 (Network: java-lambda/ver. 2)

--- day21/Main.java
+++ day21/Main.java
@@ -1,14 +1,14 @@
 /**
  * 도전 과제 01 ~ 03번은 번호 순서대로 버전 업 되는 방식입니다.<br>
  * 따라서 여러 파일을 쓰지 않고, 각 문제에 대한 답을 별도의 버전으로 commit 하였습니다.<p>
- * 현재 버전은 <b>1번 문제</b>에 대한 답입니다.
+ * 현재 버전은 <b>2번 문제</b>에 대한 답입니다.
  */
 public class Main {
     public static void main(String[] args) {
         System.out.println("사각형 : "
-                + ShapeTest.findShapesByType(Shape.shapes, "사각형"));
+                + ShapeTest.findShapes(Shape.shapes, "사각형", null, 0.));
 
         System.out.println("빨간 도형(면적<=12.0) : "
-                + ShapeTest.findShapesByColorNArea(Shape.shapes, "빨간색", 12.0));
+                + ShapeTest.findShapes(Shape.shapes, null, "빨간색", 12.0));
     }
 }
--- day21/ShapeTest.java
+++ day21/ShapeTest.java
@@ -4,20 +4,26 @@ import java.util.List;
 /**
  * 도전 과제 01 ~ 03번은 번호 순서대로 버전 업 되는 방식입니다.<br>
  * 따라서 여러 파일을 쓰지 않고, 각 문제에 대한 답을 별도의 버전으로 commit 하였습니다.<p>
- * 현재 버전은 <b>1번 문제</b>에 대한 답입니다.
+ * 현재 버전은 <b>2번 문제</b>에 대한 답입니다.
  */
 public class ShapeTest {
-    static List<Shape> findShapesByType(List<Shape> shapes, String type){
+    /**
+     * 주어진 조건을 만족하는 도형들을 출력합니다.<br>
+     * <i>null</i> 이 들어가는 조건은 비교하지 않습니다.
+     */
+    static List<Shape> findShapes(List<Shape> shapes, String type, String color, double area) {
         ArrayList<Shape> ret = new ArrayList<>();
-        for(Shape s : shapes)
-            if(s.getType().equals(type)) ret.add(s);
+        boolean match;
+
+        for (Shape s : shapes) {
+            match = true;
+            if (type != null) match = match && s.getType().equals(type);
+            if (color != null) match = match && s.getColor().equals(color);
+            if (area > 0.) match = match && s.getArea() <= area;
+
+            if (match) ret.add(s);
+        }
         return ret;
     }
 
-    static List<Shape> findShapesByColorNArea(List<Shape> shapes, String color, double area){
-        ArrayList<Shape> ret = new ArrayList<>();
-        for(Shape s : shapes)
-            if(s.getColor().equals(color) && s.getArea() <= area) ret.add(s);
-        return ret;
-    }
 }
