36e4c6850a9b6b8ee017da9e04240fc6155f5121
Cayley <cayley@dr.com>
2023-02-14 18:51:18 +0900

    HW (thread/ver 3)

--- day22/Main.java
+++ day22/Main.java
@@ -1,12 +1,16 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
- * 이 버전은 문제 2번에 대한 답입니다.<br>
+ * 이 버전은 문제 3번에 대한 답입니다.<br>
  * 다른 문제들에 대한 답은 다른 버전으로 commit 하였습니다.
  */
 public class Main {
     public static void main(String[] args) {
         int alphabet = 'a';
 
-        Thread w = new Thread(() -> {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        Runnable task = () -> {
             for (int i = 0; i <= 4; ++i) {
                 System.out.println(i);
                 try {
@@ -15,11 +19,12 @@ public class Main {
                     throw new RuntimeException(e);
                 }
             }
-        });
+            exec.shutdown();
+        };
 
-        w.start();
+        exec.execute(task);
 
-        for(; alphabet <= 'z' && w.isAlive(); alphabet++) {
+        for(; alphabet <= 'z' && !exec.isShutdown(); alphabet++) {
             System.out.println((char) alphabet);
             try {
                 Thread.sleep(500);
